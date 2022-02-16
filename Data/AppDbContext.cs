1ef15826528fe9c059b0c233d54e08cf9748235c
Humberto <jairhumberto@gmail.com>
2022-02-16 02:05:09 -0300

    Inclusao da db context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+using OperacoesService.Models;
+
+namespace OperacoesService.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) : base(opt)
+        {
+
+        }
+
+        public DbSet<Operacao> Operacoes { get; set; }
+    }
+}
