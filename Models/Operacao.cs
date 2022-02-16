474accb67e715ebcfb0ff21930e555283acd77de
Humberto <jairhumberto@gmail.com>
2022-02-16 02:02:45 -0300

    Adicao das annotations para validacao dos dados

--- Models/Operacao.cs
+++ Models/Operacao.cs
@@ -1,11 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace OperacoesService.Models
 {
     public class Operacao
     {
+        [Key, Required]
         public int Id { get; set; }
+
+        [Required]
         public string ContaNumero { get; set; }
+
+        [Required]
         public string Tipo { get; set; }
+
+        [Required]
         public decimal Valor { get; set; }
+
+        [Required]
         public DateTime DataHora { get; set; }
     }
 }
